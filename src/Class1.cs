962ffff59487e518a5c6c1b22b328e3ab07aea39
TUMBO CYRIL <cyriltumbo@gmail.com>
2019-05-25 12:21:37 +0300

    Yeah

---
+++ src/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Employees
+{
+    public class Employees
+    {
+        public string manager_id;
+        public string employee_id;
+        public string employee_salary;
+
+        public Employees(String input)
+        {
+            CSVHelper csv = new CSVHelper(input);
+            foreach(string [] t in csv)
+            {
+                this.employee_id = t[0];
+                this.manager_id = t[1];
+                this.employee_salary = t[2];
+
+            }
+
+        }
+
+    }
+}
+public class CSVHelper :
+    List<String[]>
+{
+    protected String csv = String.Empty;
+    protected String seperator = ",";
+
+    public CSVHelper (String csv, String separator = "\",\"")
+    {
+        this.csv = csv;
+        this.seperator = seperator;
+
+        foreach (string Line in Regex.Split(csv, System.Environment.NewLine).ToList().Where(String => !String.IsNullOrEmpty(String)))
+        {
+
+            String[] values = Regex.Split(Line, seperator);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                //Trim Values
+                values[i] = values[i].Replace("\"," ");
+            }
+            this.Add(values);
+        }
+    }
+}
